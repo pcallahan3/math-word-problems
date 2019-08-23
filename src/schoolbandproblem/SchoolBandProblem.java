ffbe99f0e7e3c323fbc9eec8dc24b663fe2a3891
Patrick <pcallahan3@mail.greenriver.edu>
2019-08-22 20:14:53 -0700

    Created a global Scanner object and a prompt for Tuesday minutes

--- src/schoolbandproblem/SchoolBandProblem.java
+++ src/schoolbandproblem/SchoolBandProblem.java
@@ -11,15 +11,20 @@ import java.util.Scanner;
  */
 public class SchoolBandProblem {
 	
+		private static Scanner input  = new Scanner(System.in);
+	
 		/* Create a schoolBand method to prompt user for the minutes for Monday and Tuesday
 		 * add the minutes together and output total.
 		 */
 		public void schoolBand() {
 		
-		System.out.println("Enter the number of minutes for Monday:");
-		Scanner scn = new Scanner(System.in);
-	    int monday = scn.nextInt();
-	    System.out.println("");
+			System.out.println("Enter the number of minutes for Monday:");
+		    int monday = input.nextInt();
+		    System.out.println("");
+		    
+		    System.out.println("Enter the number of minutes for Tuesday:");
+		    int tuesday = input.nextInt();
+		    System.out.println("");
 		
 		
 	}
