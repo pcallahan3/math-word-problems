37ce956f073eeb080009c11fb0e5589290570ef3
Patrick <pcallahan3@mail.greenriver.edu>
2019-08-22 20:31:45 -0700

    Added and outputted total minutes

--- src/schoolbandproblem/SchoolBandProblem.java
+++ src/schoolbandproblem/SchoolBandProblem.java
@@ -12,25 +12,31 @@ import java.util.Scanner;
 public class SchoolBandProblem {
 	
 		private static Scanner input  = new Scanner(System.in);
+		int totalMinutes;
+		
+		
 	
 		/* Create a schoolBand method to prompt user for the minutes for Monday and Tuesday
 		 * add the minutes together and output total.
 		 */
 		public void schoolBand() {
 		
-			System.out.println("Enter the number of minutes for Monday:");
+		    System.out.println("Enter the number of minutes for Monday:");
 		    int monday = input.nextInt();
 		    System.out.println("");
 		    
 		    System.out.println("Enter the number of minutes for Tuesday:");
 		    int tuesday = input.nextInt();
 		    System.out.println("");
-		
-		
+		    totalMinutes = monday + tuesday;
+		    System.out.println("Total minutes for Monday and Tuesday is:" + totalMinutes);
+		    
 	}
 	
 
 	public static void main(String[] args) {
+		SchoolBandProblem test1 = new SchoolBandProblem();
+		test1.schoolBand();
 	
 
 	}
