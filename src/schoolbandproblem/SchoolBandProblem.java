39f81035ff158c2bccd2ff932b0e3f1ec47679e8
Patrick <pcallahan3@mail.greenriver.edu>
2019-08-22 20:08:23 -0700

    Created a schoolBand() to prompt user and calculate minutes

--- src/schoolbandproblem/SchoolBandProblem.java
+++ src/schoolbandproblem/SchoolBandProblem.java
@@ -1,9 +1,32 @@
 package schoolbandproblem;
 
+import java.util.Scanner;
+
+/*
+ * ---------------QUESTION---------
+ * 
+ * The school band is getting ready for a concert.
+   They practiced 115 minutes on Monday and 125 minutes on Tuesday.
+   How many minutes did the band practice on both days?
+ */
 public class SchoolBandProblem {
+	
+		/* Create a schoolBand method to prompt user for the minutes for Monday and Tuesday
+		 * add the minutes together and output total.
+		 */
+		public void schoolBand() {
+		
+		System.out.println("Enter the number of minutes for Monday:");
+		Scanner scn = new Scanner(System.in);
+	    int monday = scn.nextInt();
+	    System.out.println("");
+		
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
