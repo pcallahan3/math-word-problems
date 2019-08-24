8f9894e37dc5c7d15a2b3f9a776c429626b9236d
Patrick <pcallahan3@mail.greenriver.edu>
2019-08-24 08:48:10 -0700

    Created calculateMinutesInt() that calculates and returns an int data type

--- src/schoolbandproblem/SchoolBandProblem.java
+++ src/schoolbandproblem/SchoolBandProblem.java
@@ -11,8 +11,6 @@ import java.util.Scanner;
  */
 public class SchoolBandProblem {
 	
-		private static Scanner input  = new Scanner(System.in);
-		int totalMinutes;
 		
 		
 	
@@ -20,20 +18,34 @@ public class SchoolBandProblem {
 		 * add the minutes together and output total.
 		 */
 		public void schoolBand() {
+			
+			Scanner input  = new Scanner(System.in);
+			int totalMinutes;
+			int monday;
+			int tuesday;
+			
 		
 		    System.out.println("Enter the number of minutes for Monday:");
-		    int monday = input.nextInt();
+		    monday = input.nextInt();
 		    System.out.println("");
 		    
 		    System.out.println("Enter the number of minutes for Tuesday:");
-		    int tuesday = input.nextInt();
+		    tuesday = input.nextInt();
 		    System.out.println("");
 		    totalMinutes = monday + tuesday;
 		    System.out.println("Total minutes for Monday and Tuesday is:" + totalMinutes);
+		    System.out.println("Total minutes for Monday and Tuesday from the calculateMinutesInt() is :" + calculateMinutesInt(monday, tuesday));
 		    
-	}
+		    
+		}
+		
+		//Calculate minutes with an int return type
+		public int calculateMinutesInt(int monday, int tuesday){
+			return monday + tuesday;
+			
+		}
+		
 	
-
 	public static void main(String[] args) {
 		SchoolBandProblem test1 = new SchoolBandProblem();
 		test1.schoolBand();
